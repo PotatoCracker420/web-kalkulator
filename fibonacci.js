df09c5435bd0d22d3e39fa7b16a880696a9ccb66
PotatoCracker420 <edu6937995@agderskolen.no>
2025-03-25 12:27:14 +0100

    fib

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,10 @@
-// fibonacci.js
-export function fibonacci(n) {
-    if (n <= 1) return n;
+// Fibonacci funksjon
+function fibonacci(n) {
+    if (n > 5) {
+        return "Maks 5 er tillatt";
+    }
+    if (n <= 2) {
+        return 1;
+    }
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
