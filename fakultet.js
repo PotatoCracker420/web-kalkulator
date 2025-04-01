4691cd1e32f169e5a4fdeb83221799267d306ce8
PotatoCracker420 <edu6937995@agderskolen.no>
2025-04-01 10:25:25 +0200

    Update fakultet.js

--- fakultet.js
+++ fakultet.js
@@ -1,3 +1,13 @@
 export function fakultet(tall) {
-    return 120 // Not-yet-implemented
+    if (tall < 0) {
+        return -1; // Factorial is not defined for negative numbers
+    } else if (tall === 0) {
+        return 1; // The factorial of 0 is 1
+    } else {
+        let result = 1;
+        for (let i = 1; i <= tall; i++) {
+            result *= i;
+        }
+        return result;
+    }
 }
